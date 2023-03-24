2398ca2a31ad2746285f2a0735fdbc07f1e658d6
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-03-24 01:11:13 +0100

    update routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import { Box } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import { SignIn } from "./Components/Authentication/SignIn";
+import Dashboard from "./Components/Dashboard/dashboard";
+import { Student } from "./Components/Dashboard/Sections/Student";
 
 function App() {
   return (
     <Box>
       <Routes>
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="student" element={<Student />} />
+        </Route>
       </Routes>
     </Box>
   );
