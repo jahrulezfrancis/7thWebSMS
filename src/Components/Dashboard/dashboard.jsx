afa1dbe1225d08f9e134ec61861ddfbe766d818b
jahrulezfrancis <longsaarmuknaan@gmail.com>
2023-04-30 20:29:03 +0100

    changed dashboard component

--- src/Components/Dashboard/dashboard.jsx
+++ src/Components/Dashboard/dashboard.jsx
@@ -1,11 +1,13 @@
 import { Box, Flex } from "@chakra-ui/react";
 import { SideBar } from "./Sidebar";
+import DashboardComponent from "./Sections/dashboardComponent";
 
 export default function Dashboard() {
     return (
-        <Box>
-            <Flex>
+        <Box background="#F0F1F3">
+            <Flex gap="10px">
                 <SideBar />
+                <DashboardComponent />
             </Flex>
         </Box>
     )
